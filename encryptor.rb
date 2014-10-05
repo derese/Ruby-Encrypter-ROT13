a801ad212dc2c7ecefaae3fdbdc9e8bff8617d6b
derese <abyssinianstar@hotmail.com>
2014-10-05 18:22:47 +0300

    encrypt message defined

--- encryptor.rb
+++ encryptor.rb
@@ -1,1 +1,16 @@
-#encryptor.rb
+class Encryptor
+  #first lets build the hash to use as a look up table for our encryption
+  def cipher
+    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+     'y' => 'l', 'z' => 'm'}
+  end
+
+  def encrypt(letter)
+    cipher[letter];
+  end
+end
